7f5bae6084bbe442b826a5b83eb2dfe6785a330e
roger <climberwoodi@gmx.ch>
2021-03-02 08:46:25 +0100

    chg: removed entities description.

--- orbis_plugin_storage_html_pages/templates/gold_entities.py
+++ orbis_plugin_storage_html_pages/templates/gold_entities.py
@@ -1,6 +1,6 @@
 gold_entities = """
 <div class="col-md-6 gold">
-    <h2>Gold Entities</h2>
+    <h2>Gold</h2>
     <p>{gold_entities_html}</p>
 </div>
 """
--- orbis_plugin_storage_html_pages/templates/predicted_entities.py
+++ orbis_plugin_storage_html_pages/templates/predicted_entities.py
@@ -1,6 +1,6 @@
 predicted_entities = """
 <div class="col-md-6 computed">
-    <h2>Predicted Entities</h2>
+    <h2>Predicted</h2>
     <p>{predicted_entities_html}</p>
 </div>
 """
